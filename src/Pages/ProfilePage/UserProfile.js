d3a7528983a7c45893edb27de643b79adc7bb37b
Amy-tech <64307770+Amy-tech@users.noreply.github.com>
2022-08-03 14:34:49 +0200

    Uploading profile image and changed name is completed (#23)

--- src/Pages/ProfilePage/UserProfile.js
+++ src/Pages/ProfilePage/UserProfile.js
@@ -1,12 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
 
-import { doc, updateDoc, getDoc } from "firebase/firestore";
-
-// import { storage } from "../../utils/init-firebase";
-// import { getDownloadURL, ref, uploadBytesResumable } from "@firebase/storage";
-// import { updateProfile } from "@firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
+import { updateProfile } from "firebase/auth";
+import { getDownloadURL, ref, uploadBytesResumable } from "@firebase/storage";
 
 import defaultPlaceholder from "../../images/placeholder.jpg";
 import typography from "../../Components/Global Components/Global Sass/Typography.module.scss";
@@ -14,17 +11,25 @@ import button from "../../Components/Global Components/Buttons/Button.module.scs
 import classes from "./UserProfile.module.scss";
 import { ImPencil2 } from "react-icons/im";
 
-import UserStorage from "../../utils/UserStorage.js"; //Importing pages dont get Brackets
 import { firestore } from "../../utils/init-firebase";
+import { auth } from "../../utils/init-firebase";
+import { storage } from "../../utils/init-firebase";
+
+import NameChangeForm from "./NameChangeForm.js";
 
 const UserProfile = () => {
   // STATE
   const [profileImage, setProfileImage] = useState(defaultPlaceholder);
-  const [showUpload, setShowUpload] = useState(false);
-  const [imageProgress, setImageProgress] = useState(0);
-  // const [allDocs, setAllDocs] = useState([]);
+  const [showProgressCounter, setShowProgressCounter] = useState(null);
+  const [imageProgress, setImageProgress] = useState(false);
+  const [newName, setNewName] = useState(false);
 
-  const authData = useSelector((state) => state.auth);
+  //REFS
+  // const updatedDisplayNameInputRef = useRef();
+
+  // FETCHING CURRENT USER INFORMATION
+  const currentUser = auth.currentUser;
+  // console.log(currentUser); //this works
 
   // PROFILE IMAGE HANDLER
   const imageEditHandler = (e) => {
@@ -36,56 +41,75 @@ const UserProfile = () => {
   // PROFILE IMAGE LOGIC
   const imageUploadHandler = (file) => {
     if (!file) return;
-    // const documentName = authData.user.idToken;
-    // console.log(documentName);
 
-    // ADDING IMAGE LOADING COUNTER
+    // ADDING PROFILE IMAGE TO FIREBASE AUTH
+    updateProfile(currentUser, {
+      photoURL: file.name,
+    });
 
     // ADDING PROFILE IMAGE TO FIRESTORE
-    const profileImageRef = doc(firestore, "usersList", authData.user.idToken);
-    updateDoc(profileImageRef, {
-      profileImage: file.name,
-    }); //this only work if the user registers, but not when the user logs in again there after!
-    // gives error ==> "uncaught (in promise) firebaseError: No document to update..."
+    const photoURLRef = doc(firestore, "usersList", currentUser.uid);
+    updateDoc(photoURLRef, {
+      photoURL: file.name,
+    });
 
-    // ** THIS IS STORAGE CODE **
-    // if (currentProfileImageSnap) {
-    //   console.log(currentProfileImageSnap.data());
-    // } else {
-    //   console.log("no such document");
-    // }
+    // ADDING PROFILE IMAGE TO CURRENT USER STORAGE
+    const storageRef = ref(storage, `/${currentUser.uid}/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    setShowProgressCounter(true);
 
-    // this currently stores all images to firebase storage
-    // const storageRef = ref(storage, `/files/${file.name}`);
-    // const uploadTask = uploadBytesResumable(storageRef, file);
-    // setShowUpload(true);
+    // TRACKING PROGRESS
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
 
-    // uploadTask.on(
-    //   "state_changed",
-    //   (snapshot) => {
-    //     const progress = Math.round(
-    //       (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-    //     );
+        setImageProgress(progress);
+      },
+      (err) => console.log(err),
 
-    //     setImageProgress(progress);
-    //   },
-    //   (err) => console.log(err),
+      // FETCHING CHANGED IMAGE
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          setProfileImage(url);
+          setShowProgressCounter(false);
+        });
+      }
+    );
 
-    //   () => {
-    //     getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-    //       setProfileImage(url);
-    //       setShowUpload(false);
-    //     });
-    //   }
-    // );
+    file && updateProfile(currentUser.uid, { photoURL: profileImage });
+  };
 
-    // updateProfile(authData.user.idToken, { photoURL: profileImage });
+  // FETCHING CURRENT IMAGE
+  const photoRef = ref(storage, `/${currentUser.uid}/${currentUser.photoURL}`);
+
+  // USE EFFECT
+  useEffect(() => {
+    const fetchingImage = async () => {
+      const image = await getDownloadURL(photoRef);
+      setProfileImage(image);
+    };
+
+    fetchingImage();
+  }, [photoRef]);
+
+  // CHANGING USER NAME
+  const nameEditHandler = () => {
+    // TOGGLE NAME INPUT
+    if (newName === false) {
+      setNewName(true);
+    } else {
+      setNewName(false);
+    }
   };
 
   return (
     <div className={classes.profile}>
       <header className={classes.profile__heading}>
         <div className={classes.profile__heading_edit}>
+          {/* EDIT PROFILE IMAGE BTN */}
           <input
             type="file"
             name="image-upload"
@@ -94,50 +118,57 @@ const UserProfile = () => {
             onChange={imageEditHandler}
             className={classes.profile__heading_editInput}
           />
-
-          {/* EDIT BTN */}
           <label htmlFor="input" className={classes.profile__heading_editLabel}>
             <ImPencil2 />
           </label>
         </div>
 
-        {/* USER PROFILE IMAGE */}
+        {/* DISPLAY PROFILE IMAGE */}
         <div className={classes.profile__heading_userPic}>
           <img
             src={profileImage}
-            alt="avitar placeholder"
+            alt="Avatar"
             className={classes.profile__heading_avitar}
           ></img>
         </div>
-        {showUpload && (
+
+        {/* UPLOAD COUNTER */}
+        {showProgressCounter && (
           <h1 className={classes.profile__heading_uploading}>
             Uploading {imageProgress} %
           </h1>
         )}
 
-        <div className={classes.profile__heading_userDetails}>
-          {/* SUBHEADING - USER NAME */}
-          <p
-            className={`${typography.paragraph} ${classes.profile__heading_subHeading}`}
-          >
-            User Name :
-          </p>
+        {/* REMEMBER CONTINUE ON THIS SPOT */}
+        {newName ? (
+          <NameChangeForm />
+        ) : (
+          <div className={classes.profile__heading_userDetails}>
+            {/* SUBHEADING - USER NAME */}
+            <p
+              className={`${typography.paragraph} ${classes.profile__heading_subHeading}`}
+            >
+              User Name :
+            </p>
 
-          {/* USER NAME */}
-          <p
-            className={`${typography.paragraph} ${classes.profile__heading_userName}`}
-          >
-            {authData.user.displayName}
-          </p>
-        </div>
+            {/* USER NAME */}
+            <p
+              className={`${typography.paragraph} ${classes.profile__heading_userName}`}
+            >
+              {currentUser.displayName}
+            </p>
+          </div>
+        )}
+        {/* TOGGLE CHANGE USER NAME INPUT */}
 
         {/* SUBHEADING - NAVIGATION BTNS */}
         <div className={classes.profile__heading_btnContainer}>
           <NavLink
             to={"#"}
+            onClick={nameEditHandler}
             className={`${button.btn__secondary} ${button.btn} ${classes.profile__heading_btnContainer_btn1}`}
           >
-            change password
+            change username
           </NavLink>
 
           <NavLink
@@ -157,7 +188,7 @@ const UserProfile = () => {
       </header>
 
       {/* BODY - USER RECIPE HOLDER */}
-      <body className={classes.profile__body}>
+      <div className={classes.profile__body}>
         <div className={classes.profile__empty}>
           <p className={typography.paragraph}>
             No Items in profile.
@@ -174,8 +205,7 @@ const UserProfile = () => {
             </NavLink>
           </nav>
         </div>
-        <UserStorage />
-      </body>
+      </div>
     </div>
   );
 };
